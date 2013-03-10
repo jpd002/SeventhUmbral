19038cb23cd3076574ff0aaa9e6f752192164ab9
jpd002 <jpd002@b2dc461f-b7ed-4520-8422-8e9c2511a46d>
2013-03-10 07:22:17 +0000

    Added PacketUnpacker tool.
    git-svn-id: http://svn.purei.org/seventhumbral/trunk@16 b2dc461f-b7ed-4520-8422-8e9c2511a46d

---
+++ tools/PacketUnpacker/MainForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace PacketUnpacker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputTextBox = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputTextBox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(0, 0);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.inputTextBox.Size = new System.Drawing.Size(798, 223);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            // 
+            // outputTextBox
+            // 
+            this.outputTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputTextBox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputTextBox.Location = new System.Drawing.Point(0, 0);
+            this.outputTextBox.Multiline = true;
+            this.outputTextBox.Name = "outputTextBox";
+            this.outputTextBox.ReadOnly = true;
+            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputTextBox.Size = new System.Drawing.Size(798, 219);
+            this.outputTextBox.TabIndex = 2;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.inputTextBox);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.outputTextBox);
+            this.splitContainer1.Size = new System.Drawing.Size(798, 446);
+            this.splitContainer1.SplitterDistance = 223;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 446);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "MainForm";
+            this.Text = "FFXIV Packet Unpacker";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+    }
+}
+
---
+++ tools/PacketUnpacker/MainForm.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.IO.Compression;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PacketUnpacker
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            outputTextBox.Text = UnpackPacket(inputTextBox.Text);
+        }
+
+        private void inputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            outputTextBox.Text = UnpackPacket(inputTextBox.Text);
+        }
+
+        private string PrintStreamContents(Stream stream)
+        {
+            var result = new StringBuilder();
+
+            var outputBuffer = new byte[0x10];
+
+            while (true)
+            {
+                int readAmount = stream.Read(outputBuffer, 0, outputBuffer.Length);
+                if (readAmount == 0) break;
+                for (int i = 0; i < readAmount; i++)
+                {
+                    result.AppendFormat("{0:x2} ", outputBuffer[i]);
+                }
+                for (int i = readAmount; i < 0x10; i++)
+                {
+                    result.Append("   ");
+                }
+                result.Append("         ");
+                for (int i = 0; i < readAmount; i++)
+                {
+                    byte inputByte = outputBuffer[i];
+                    char outputChar = '.';
+                    if (char.IsLetterOrDigit((char)inputByte))
+                    {
+                        outputChar = (char)inputByte;
+                    }
+                    result.AppendFormat("{0}", outputChar);
+                }
+                result.AppendFormat("\r\n");
+            }
+
+            return result.ToString();
+        }
+
+        private string PrintPacketCommands(Stream stream)
+        {
+            var result = new StringBuilder();
+
+            while (true)
+            {
+                var header = new byte[0x10];
+                var headerReadSize = stream.Read(header, 0, header.Length);
+                if (headerReadSize != 0x10) break;
+                stream.Seek(-0x10, SeekOrigin.Current);
+
+                UInt16 commandSize = (UInt16)((header[0]) | (header[1] << 8));
+                var commandData = new byte[commandSize];
+                stream.Read(commandData, 0, commandData.Length);
+
+                result.AppendFormat("Command Size: {0} bytes\r\n", commandSize);
+                using (var commandDataStream = new MemoryStream(commandData))
+                {
+                    result.AppendFormat(PrintStreamContents(commandDataStream));
+                }
+                result.Append("\r\n");
+            }
+
+            return result.ToString();
+        }
+
+        private string UnpackPacket(string inputString)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(inputString) || (inputString.Length & 1) != 0)
+                {
+                    throw new Exception("Invalid input packet.");
+                }
+
+                using (var packetDataStream = new MemoryStream())
+                {
+                    for (int i = 0; i < inputString.Length / 2; i++)
+                    {
+                        string byteValString = new string(new char[] { inputString[(i * 2) + 0], inputString[(i * 2) + 1] });
+                        uint byteVal = Convert.ToUInt32(byteValString, 16);
+                        packetDataStream.WriteByte((byte)byteVal);
+                    }
+
+                    packetDataStream.Seek(0, SeekOrigin.Begin);
+
+                    var result = new StringBuilder();
+
+                    while (true)
+                    {
+                        var header = new byte[0x10];
+                        var headerReadSize = packetDataStream.Read(header, 0, header.Length);
+                        if (headerReadSize != 0x10) break;
+
+                        UInt16 subPacketSize = (UInt16)((header[4]) | (header[5] << 8));
+
+                        result.AppendFormat("Sub Packet Size: {0} bytes\r\n\r\n", subPacketSize);
+
+                        var subPacketData = new byte[subPacketSize - 0x10];
+                        packetDataStream.Read(subPacketData, 0, subPacketData.Length);
+
+                        using (MemoryStream compressedSubPacketDataStream = new MemoryStream(subPacketData))
+                        {
+                            compressedSubPacketDataStream.ReadByte();
+                            compressedSubPacketDataStream.ReadByte();
+
+                            using (var deflateStream = new DeflateStream(compressedSubPacketDataStream, CompressionMode.Decompress))
+                            {
+                                using (var subPacketDataStream = new MemoryStream())
+                                {
+                                    var buffer = new byte[0x200];
+                                    while (true)
+                                    {
+                                        int readAmount = deflateStream.Read(buffer, 0, buffer.Length);
+                                        if (readAmount == 0) break;
+                                        subPacketDataStream.Write(buffer, 0, readAmount);
+                                    }
+
+                                    subPacketDataStream.Seek(0, SeekOrigin.Begin);
+
+                                    result.Append(PrintPacketCommands(subPacketDataStream));
+                                    result.Append("-------------------------------------------------------------------------------\r\n");
+                                }
+                            }
+                        }
+                    }
+
+                    return result.ToString();
+                }
+            }
+            catch (Exception exception)
+            {
+                return exception.Message;
+            }
+        }
+    }
+}
