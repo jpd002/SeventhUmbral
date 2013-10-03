5bd6e45ede4eab17c9146acac7bdb3abdf7a064c
jpd002 <jpd002@b2dc461f-b7ed-4520-8422-8e9c2511a46d>
2013-10-03 06:23:48 +0000

    Added database creation script.
    git-svn-id: http://svn.purei.org/seventhumbral/trunk@58 b2dc461f-b7ed-4520-8422-8e9c2511a46d

---
+++ common/CreateDatabase.sql
@@ -0,0 +1,24 @@
+delimiter $$
+
+CREATE DATABASE `ffxiv` /*!40100 DEFAULT CHARACTER SET utf8 */$$
+
+USE ffxiv;
+
+CREATE TABLE `ffxiv_users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `passhash` char(56) NOT NULL,
+  `salt` char(56) NOT NULL,
+  `email` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8$$
+
+CREATE TABLE `ffxiv_sessions` (
+  `id` char(56) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `expiration` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `userid_UNIQUE` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8$$
+
