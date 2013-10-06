0e84edfceaf2cf028614ebb6100b370ab7637a12
jpd002 <jpd002@b2dc461f-b7ed-4520-8422-8e9c2511a46d>
2013-10-06 06:37:54 +0000

    Changed subtitle display (display only server name). Ported Chrome/Safari fix from website.
    git-svn-id: http://svn.purei.org/seventhumbral/trunk@67 b2dc461f-b7ed-4520-8422-8e9c2511a46d

--- website/header.php
+++ website/header.php
@@ -1,6 +1,6 @@
 <header class="top">
 	<div class="inner">
 		<p class="pageTitle">Seventh Umbral Server<p>
-		<p class="pageSubTitle"><?php echo $server_name; ?> - Administration Site</p>
+		<p class="pageSubTitle"><?php echo $server_name; ?></p>
 	</div>
 </header>
