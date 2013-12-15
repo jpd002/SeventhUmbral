2ed7c101cce7a292b3c968254b6159f9c08c9585
jpd002 <jpd002@b2dc461f-b7ed-4520-8422-8e9c2511a46d>
2013-12-15 06:54:24 +0000

    Launcher v1.01
    git-svn-id: http://svn.purei.org/seventhumbral/trunk@92 b2dc461f-b7ed-4520-8422-8e9c2511a46d

--- launcher/AppDef.h
+++ launcher/AppDef.h
@@ -3,8 +3,8 @@
 //Don't put tabs here since it will screw up the installer package
 
 #define APP_NAME _T("Seventh Umbral Launcher")
-#define APP_VERSION (100)
-#define APP_VERSIONSTR _T("1.00")
+#define APP_VERSION (101)
+#define APP_VERSIONSTR _T("1.01")
 
 #define FFXIV_BOOT_VERSION ("2010.09.18.0000")
 #define FFXIV_GAME_VERSION ("2012.09.19.0001")
