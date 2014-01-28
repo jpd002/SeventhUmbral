8341361fc0146a194cac72fd6a34f8225479a073
jpd002 <jpd002@b2dc461f-b7ed-4520-8422-8e9c2511a46d>
2014-01-28 06:25:13 +0000

    Added reading of parameters in parameter chunks.
    git-svn-id: http://svn.purei.org/seventhumbral/trunk@124 b2dc461f-b7ed-4520-8422-8e9c2511a46d

--- tools/WorldEditor/Source/PramChunk.cpp
+++ tools/WorldEditor/Source/PramChunk.cpp
@@ -39,16 +39,70 @@ void CPramChunk::Read(Framework::CStream& inputStream)
 	auto samplerDataOffsets = ReadOffsets(inputStream, m_numSamplers);
 	auto samplerNameOffsets = ReadOffsets(inputStream, m_numSamplers);
 
+	ReadParameters(inputStream, basePosition, parameterDataOffsets, parameterNameOffsets);
+	ReadSamplers(inputStream, basePosition, samplerDataOffsets, samplerNameOffsets);
+}
+
+const CPramChunk::ParameterArray& CPramChunk::GetParameters() const
+{
+	return m_parameters;
+}
+
+const CPramChunk::SamplerArray& CPramChunk::GetSamplers() const
+{
+	return m_samplers;
+}
+
+CPramChunk::OffsetArray CPramChunk::ReadOffsets(Framework::CStream& inputStream, uint32 itemCount)
+{
+	OffsetArray result;
+	result.resize(itemCount);
+	inputStream.Read(result.data(), itemCount * sizeof(uint32));
+	return result;
+}
+
+void CPramChunk::ReadParameters(Framework::CStream& inputStream, uint32 basePosition, const OffsetArray& dataOffsets, const OffsetArray& nameOffsets)
+{
+	m_parameters.resize(m_numParameters);
+
+	for(uint32 i = 0; i < m_numParameters; i++)
+	{
+		const auto& dataOffset = dataOffsets[i];
+		const auto& nameOffset = nameOffsets[i];
+		auto& parameter = m_parameters[i];
+
+		auto absDataOffset = basePosition + dataOffset;
+		auto absNameOffset = basePosition + nameOffset;
+
+		inputStream.Seek(absDataOffset, Framework::STREAM_SEEK_SET);
+		parameter.isPixelShaderParam = inputStream.Read8() != 0;
+		parameter.unknown2 = inputStream.Read8();
+		parameter.numValues = inputStream.Read8();
+		parameter.unknown3 = inputStream.Read8();
+		parameter.unknown4 = inputStream.Read32();
+		parameter.unknown5 = inputStream.Read32();
+		parameter.unknown6 = inputStream.Read32();
+		parameter.valueX = inputStream.ReadFloat32();
+		parameter.valueY = inputStream.ReadFloat32();
+		parameter.valueZ = inputStream.ReadFloat32();
+		parameter.valueW = inputStream.ReadFloat32();
+		inputStream.Seek(absNameOffset, Framework::STREAM_SEEK_SET);
+		parameter.name = inputStream.ReadString();
+	}
+}
+
+void CPramChunk::ReadSamplers(Framework::CStream& inputStream, uint32 basePosition, const OffsetArray& dataOffsets, const OffsetArray& nameOffsets)
+{
 	m_samplers.resize(m_numSamplers);
 
 	for(uint32 i = 0; i < m_numSamplers; i++)
 	{
-		const auto& samplerDataOffset = samplerDataOffsets[i];
-		const auto& samplerNameOffset = samplerNameOffsets[i];
+		const auto& dataOffset = dataOffsets[i];
+		const auto& nameOffset = nameOffsets[i];
 		auto& sampler = m_samplers[i];
 
-		auto absDataOffset = basePosition + samplerDataOffset;
-		auto absNameOffset = basePosition + samplerNameOffset;
+		auto absDataOffset = basePosition + dataOffset;
+		auto absNameOffset = basePosition + nameOffset;
 
 		inputStream.Seek(absDataOffset, Framework::STREAM_SEEK_SET);
 
@@ -74,17 +128,4 @@ void CPramChunk::Read(Framework::CStream& inputStream)
 		inputStream.Seek(absNameOffset, Framework::STREAM_SEEK_SET);
 		sampler.name = inputStream.ReadString();
 	}
-}
-
-const CPramChunk::SamplerArray& CPramChunk::GetSamplers() const
-{
-	return m_samplers;
-}
-
-CPramChunk::OffsetArray CPramChunk::ReadOffsets(Framework::CStream& inputStream, uint32 itemCount)
-{
-	OffsetArray result;
-	result.resize(itemCount);
-	inputStream.Read(result.data(), itemCount * sizeof(uint32));
-	return result;
 }
--- tools/WorldEditor/Source/PramChunk.h
+++ tools/WorldEditor/Source/PramChunk.h
@@ -7,6 +7,23 @@ class CPramChunk : public CBaseChunk
 public:
 	typedef std::vector<std::string> StringTable;
 
+	struct PARAMETER
+	{
+		std::string		name;
+		bool			isPixelShaderParam = false;
+		uint8			unknown2 = 0;
+		uint8			numValues = 0;
+		uint8			unknown3 = 0;
+		uint32			unknown4 = 0;
+		uint32			unknown5 = 0;
+		uint32			unknown6 = 0;
+		float			valueX = 0;
+		float			valueY = 0;
+		float			valueZ = 0;
+		float			valueW = 0;
+	};
+	typedef std::vector<PARAMETER> ParameterArray;
+
 	struct SAMPLER
 	{
 		std::string		name;
@@ -19,12 +36,15 @@ public:
 
 	virtual void			Read(Framework::CStream&) override;
 
+	const ParameterArray&	GetParameters() const;
 	const SamplerArray&		GetSamplers() const;
 
 private:
 	typedef std::vector<uint32> OffsetArray;
 
 	OffsetArray				ReadOffsets(Framework::CStream&, uint32);
+	void					ReadParameters(Framework::CStream&, uint32, const OffsetArray&, const OffsetArray&);
+	void					ReadSamplers(Framework::CStream&, uint32, const OffsetArray&, const OffsetArray&);
 
 	uint32					m_numParameters;
 	uint32					m_parameterNameStart;
@@ -32,6 +52,7 @@ private:
 	uint32					m_samplerDataStart;
 	uint32					m_samplerNameStart;
 
+	ParameterArray			m_parameters;
 	SamplerArray			m_samplers;
 };
 
