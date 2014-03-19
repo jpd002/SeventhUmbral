66231a184fac72f677e1c89790856770ccb600c6
jpd002 <jpd002@b2dc461f-b7ed-4520-8422-8e9c2511a46d>
2014-03-19 09:26:22 +0000

    Made Dx11UmbralEffectProvider work using generalized system.
    git-svn-id: http://svn.purei.org/seventhumbral/trunk@168 b2dc461f-b7ed-4520-8422-8e9c2511a46d

--- tools/WorldEditor/Source/Rendering/Dx11UmbralEffectProvider.cpp
+++ tools/WorldEditor/Source/Rendering/Dx11UmbralEffectProvider.cpp
@@ -14,24 +14,7 @@ CDx11UmbralEffectProvider::~CDx11UmbralEffectProvider()
 
 }
 
-Athena::EffectPtr CDx11UmbralEffectProvider::GetEffect(const CD3DShader& vertexShader, const CD3DShader& pixelShader)
+Athena::EffectPtr CDx11UmbralEffectProvider::CreateEffect(const CD3DShader& vertexShader, const CD3DShader& pixelShader)
 {
-	EFFECTINFO effectInfo;
-	effectInfo.vertexShader = vertexShader;
-	effectInfo.pixelShader = pixelShader;
-	auto effectIterator = m_effects.find(effectInfo);
-	if(effectIterator != m_effects.end())
-	{
-		return effectIterator->second;
-	}
-	auto effect = std::make_shared<CDx11UmbralEffect>(m_device, m_deviceContext, vertexShader, pixelShader);
-	m_effects.insert(std::make_pair(effectInfo, effect));
-	return effect;
-}
-
-Athena::EffectPtr CDx11UmbralEffectProvider::GetEffectForRenderable(Athena::CMesh* mesh, bool hasShadowMap)
-{
-	assert(dynamic_cast<CUmbralMesh*>(mesh));
-	auto umbralMesh = static_cast<CUmbralMesh*>(mesh);
-	return umbralMesh->GetEffect();
+	return std::make_shared<CDx11UmbralEffect>(m_device, m_deviceContext, vertexShader, pixelShader);
 }
