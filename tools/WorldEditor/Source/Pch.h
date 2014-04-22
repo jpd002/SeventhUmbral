10299e931e6b0b349c1a0dcf1bedabe5779c012f
jpd002 <jpd002@b2dc461f-b7ed-4520-8422-8e9c2511a46d>
2014-04-22 06:31:24 +0000

    Added precompiled header usage in WorldEditor.
    git-svn-id: http://svn.purei.org/seventhumbral/trunk@229 b2dc461f-b7ed-4520-8422-8e9c2511a46d

---
+++ tools/WorldEditor/Source/Pch.h
@@ -0,0 +1,13 @@
+#ifndef _PCH_H_
+#define _PCH_H_
+
+#include <string>
+#include <map>
+#include <vector>
+#include <memory>
+#include <functional>
+
+#include "PalleonEngine.h"
+#include "../../../dataobjects/ResourceDefs.h"
+
+#endif
