97dd69aeca4b5b8c36a2a531f19f06d2b19aef0a
jpd002 <jpd002@b2dc461f-b7ed-4520-8422-8e9c2511a46d>
2014-07-27 06:00:53 +0000

    Seventh Umbral Workshop v0.05
    git-svn-id: http://svn.purei.org/seventhumbral/trunk@331 b2dc461f-b7ed-4520-8422-8e9c2511a46d

--- workshop/AppDef.h
+++ workshop/AppDef.h
@@ -3,5 +3,5 @@
 //Don't put tabs here since it will screw up the installer package
 
 #define APP_NAME _T("Seventh Umbral Workshop")
-#define APP_VERSION (4)
-#define APP_VERSIONSTR _T("0.04")
+#define APP_VERSION (5)
+#define APP_VERSIONSTR _T("0.05")
