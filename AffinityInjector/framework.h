ab5e0c1d6536c51c3b693aee842f873cc851dc74
Salem AL-Ghanim <salemthunder.fist@gmail.com>
2021-12-23 19:13:26 +0300

    AffinityInjector DLL

---
+++ AffinityInjector/framework.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+#include <iostream>
+#include <detours.h>
+
+#pragma comment(lib, "KERNEL32.lib")
--- launcher/Launcher.cpp
+++ launcher/Launcher.cpp
@@ -1,6 +1,9 @@
 #include <Windows.h>
 #include <algorithm>
 #include <tchar.h>
+#include <psapi.h>
+#include <strsafe.h>
+#include <Shlwapi.h>
 #include "Launcher.h"
 #include "Base64.h"
 #include "../common/BLOWFISH.H"
@@ -10,6 +13,9 @@ static const uint8 g_encryptionTimePatch[] = { 0xB8, 0x12, 0xE8, 0xE0, 0x50 };
 static const uint32 g_lobbyHostNameAddress = 0xB90110;
 
 static const unsigned int g_lobbyHostNamePatchSize = 0x14;
+
+BOOL WINAPI InjectDll(__in LPCWSTR lpcwszDll, PROCESS_INFORMATION processInformation);
+void PrintError(LPTSTR lpszFunction);
 
 static bool ApplyPatch(HANDLE hProcess, uint32 address, const uint8* patchBytes, uint32 patchSize)
 {
@@ -103,6 +109,11 @@ void CLauncher::Launch(const char* workingDirectory, const char* lobbyHostName, 
 
 	PROCESS_INFORMATION procInfo = { 0 };
 
+
+	KAFFINITY affinity = 0x00000000000000FF;
+	HANDLE currentProcess = GetCurrentProcess();
+	SetProcessAffinityMask(currentProcess, affinity);
+
 	BOOL processCreated = CreateProcessA(NULL, completeCommandLine, NULL, NULL, FALSE, CREATE_SUSPENDED | NORMAL_PRIORITY_CLASS, NULL, workingDirectory, &startupInfo, &procInfo);
 	if(!processCreated)
 	{
@@ -120,8 +131,135 @@ void CLauncher::Launch(const char* workingDirectory, const char* lobbyHostName, 
 		throw;
 	}
 
-	ResumeThread(procInfo.hThread);
+	SetProcessAffinityMask(procInfo.hProcess, affinity);
+	wchar_t selfdir[MAX_PATH] = { 0 };
+	GetModuleFileName(NULL, selfdir, MAX_PATH);
+	PathRemoveFileSpec(selfdir);
 
-	CloseHandle(procInfo.hProcess);
-	CloseHandle(procInfo.hThread);
+	std::wstring dllPath = std::wstring(selfdir) + TEXT("\\AffinityInjector.dll");
+
+
+	if (InjectDll(dllPath.c_str(), procInfo)) {
+		printf("Dll was successfully injected.\n");
+	}
+	else {
+		printf("Terminating the Injector app...");
+	}
+
+	//ResumeThread(procInfo.hThread);
+
+	//CloseHandle(procInfo.hProcess);
+	//CloseHandle(procInfo.hThread);
+}
+
+
+BOOL WINAPI InjectDll(__in LPCWSTR lpcwszDll, PROCESS_INFORMATION processInformation)
+{
+	SIZE_T nLength;
+	LPVOID lpLoadLibraryW = NULL;
+	LPVOID lpRemoteString;
+	STARTUPINFO             startupInfo;
+	//PROCESS_INFORMATION     processInformation;
+
+	memset(&startupInfo, 0, sizeof(startupInfo));
+	startupInfo.cb = sizeof(STARTUPINFO);
+
+	/*if (!CreateProcess(targetPath, NULL, NULL, NULL, FALSE,
+		CREATE_SUSPENDED, NULL, NULL, &startupInfo, &processInformation))
+	{
+		PrintError(TEXT("CreateProcess"));
+		return FALSE;
+	}*/
+
+	lpLoadLibraryW = GetProcAddress(GetModuleHandle(L"KERNEL32.DLL"), "LoadLibraryW");
+
+	if (!lpLoadLibraryW)
+	{
+		PrintError(TEXT("GetProcAddress"));
+		// close process handle
+		CloseHandle(processInformation.hProcess);
+		return FALSE;
+	}
+
+	nLength = wcslen(lpcwszDll) * sizeof(WCHAR);
+
+	// allocate mem for dll name
+	lpRemoteString = VirtualAllocEx(processInformation.hProcess, NULL, nLength + 1, MEM_COMMIT, PAGE_READWRITE);
+	if (!lpRemoteString)
+	{
+		PrintError(TEXT("VirtualAllocEx"));
+
+		// close process handle
+		CloseHandle(processInformation.hProcess);
+
+		return FALSE;
+	}
+
+	// write dll name
+	if (!WriteProcessMemory(processInformation.hProcess, lpRemoteString, lpcwszDll, nLength, NULL)) {
+
+		PrintError(TEXT("WriteProcessMemory"));
+		// free allocated memory
+		VirtualFreeEx(processInformation.hProcess, lpRemoteString, 0, MEM_RELEASE);
+
+		// close process handle
+		CloseHandle(processInformation.hProcess);
+
+		return FALSE;
+	}
+
+	// call loadlibraryw
+	HANDLE hThread = CreateRemoteThread(processInformation.hProcess, NULL, NULL, (LPTHREAD_START_ROUTINE)lpLoadLibraryW, lpRemoteString, NULL, NULL);
+
+	if (!hThread) {
+		PrintError(TEXT("CreateRemoteThread"));
+	}
+	else {
+		WaitForSingleObject(hThread, 8000);
+
+		//resume suspended process
+		ResumeThread(processInformation.hThread);
+	}
+
+	//  free allocated memory
+	VirtualFreeEx(processInformation.hProcess, lpRemoteString, 0, MEM_RELEASE);
+
+	// close process handle
+	CloseHandle(processInformation.hProcess);
+	CloseHandle(processInformation.hThread);
+
+	return TRUE;
+}
+
+void PrintError(LPTSTR lpszFunction)
+{
+	// Retrieve the system error message for the last-error code
+
+	LPVOID lpMsgBuf;
+	LPVOID lpDisplayBuf;
+	DWORD dw = GetLastError();
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER |
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		dw,
+		MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
+		(LPTSTR)&lpMsgBuf,
+		0, NULL);
+
+	// Display the error message
+
+	lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT,
+		(lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR));
+	StringCchPrintf((LPTSTR)lpDisplayBuf,
+		LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+		TEXT("%s failed with error %d: %s"),
+		lpszFunction, dw, lpMsgBuf);
+
+	wprintf(L"%s", lpDisplayBuf);
+
+	LocalFree(lpMsgBuf);
+	LocalFree(lpDisplayBuf);
 }
